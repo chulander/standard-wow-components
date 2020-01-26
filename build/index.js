06285ff498cecca3aa6a97bf8efd9408d5082004
chulander <bryan.p.chu@gmail.com>
2020-01-26 18:37:09 -0500

    updated

---
+++ build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "MagicButton", {
+  enumerable: true,
+  get: function get() {
+    return _MagicButton.MagicButton;
+  }
+});
+
+var _MagicButton = require("./MagicButton");
+//# sourceMappingURL=index.js.map
