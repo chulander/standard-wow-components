f8237b8dbd716cafa3a067f008992f0563c17f73
chulander <bryan.p.chu@gmail.com>
2020-01-26 19:46:39 -0500

    updated

--- src/MagicButton.jsx
+++ src/MagicButton.jsx
@@ -3,13 +3,6 @@ import styled from "styled-components";
 import Button from "@argodigital/ui-toolkit/dist/Button";
 
 export function MagicButton({ className, children }) {
-  const Button = useMemo(
-    () => styled.button`
-      background-color: blue;
-      color: red;
-    `,
-    []
-  );
   return (
     <Button theme="primary">
       <Button.Text>{children}</Button.Text>
